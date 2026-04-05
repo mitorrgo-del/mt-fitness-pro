8b0f7c2dd9f0e791bfe6ccb21f7dc327ffa84dc2
mitorrgo <mitorrgo@gmail.com>
2026-04-05 19:50:28 +0200

    OFFICIAL v1.2.2 GOLD ELITE - Action Exercises & Luxury Theme - Ready for Deploy

--- mt_fitness_app/lib/main.dart
+++ mt_fitness_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'theme/app_theme.dart';
 import 'screens/login_screen.dart';
 
